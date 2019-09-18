1a8a7ad1bc17e639e215e436d715fc3c0b12c787
phatdroid94 <phatdroid94@gmail.com>
2019-09-18 18:39:52 +0700

    a

--- app/src/main/java/phamtanphat/ptp/khoaphamtraining/readjson02072019/MainActivity.java
+++ app/src/main/java/phamtanphat/ptp/khoaphamtraining/readjson02072019/MainActivity.java
@@ -1,89 +1,107 @@
 package phamtanphat.ptp.khoaphamtraining.readjson02072019;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.concurrent.Callable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import io.reactivex.Observable;
-import io.reactivex.ObservableSource;
+import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.functions.Consumer;
-import io.reactivex.functions.Function;
+import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView txtJsonDemo1;
     Button btnJsonDemo1;
+    // Dai quan sat : Noi chua du lieu se phat tan ra ngoai
     Observable<String> mData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         txtJsonDemo1 = findViewById(R.id.textviewJson);
         btnJsonDemo1 = findViewById(R.id.buttonJsonDemo1);
 
-        btnJsonDemo1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                xulydocjsondemo1();
-            }
-        });
-    }
-    @SuppressLint("CheckResult")
-    private void xulydocjsondemo1(){
-        mData = Observable.defer(new Callable<ObservableSource<? extends String>>() {
-            @Override
-            public ObservableSource<? extends String> call() throws Exception {
-                return Observable.just(docNoiDung_Tu_URL("https://khoapham.vn/KhoaPhamTraining/json/tien/demo1.json"));
-            }
-        });
-
+//        btnJsonDemo1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                xulydocjsondemo1();
+//            }
+//        });
+        mData = Observable.just("Hoa","Phat","Tuan","Loi","Thuy");
         mData
-            .observeOn(AndroidSchedulers.mainThread())
             .subscribeOn(Schedulers.io())
-            .subscribe(new Consumer<String>() {
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(new Observer<String>() {
                 @Override
-                public void accept(String s) throws Exception {
+                public void onSubscribe(Disposable d) {
+
+                }
+
+                @Override
+                public void onNext(String s) {
                     Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
                 }
+
+                @Override
+                public void onError(Throwable e) {
+
+                }
+
+                @Override
+                public void onComplete() {
+
+                }
             });
+
     }
-    private String docNoiDung_Tu_URL(String theUrl){
-        StringBuilder content = new StringBuilder();
-        try    {
-            // create a url object
-            URL url = new URL(theUrl);
-
-            // create a urlconnection object
-            URLConnection urlConnection = url.openConnection();
-
-            // wrap the urlconnection in a bufferedreader
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-
-            String line;
-
-            // read from the urlconnection via the bufferedreader
-            while ((line = bufferedReader.readLine()) != null){
-                content.append(line + "\n");
-            }
-            bufferedReader.close();
-        }
-        catch(Exception e)    {
-            e.printStackTrace();
-        }
-        return content.toString();
-    }
+//    @SuppressLint("CheckResult")
+//    private void xulydocjsondemo1(){
+//        mData = Observable.defer(new Callable<ObservableSource<? extends String>>() {
+//            @Override
+//            public ObservableSource<? extends String> call() throws Exception {
+//                return Observable.just(docNoiDung_Tu_URL("https://khoapham.vn/KhoaPhamTraining/json/tien/demo1.json"));
+//            }
+//        });
+//
+//        mData
+//            .observeOn(AndroidSchedulers.mainThread())
+//            .subscribeOn(Schedulers.io())
+//            .subscribe(new Consumer<String>() {
+//                @Override
+//                public void accept(String s) throws Exception {
+//                    Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
+//                }
+//            });
+//    }
+//    private String docNoiDung_Tu_URL(String theUrl){
+//        StringBuilder content = new StringBuilder();
+//        try    {
+//            // create a url object
+//            URL url = new URL(theUrl);
+//
+//            // create a urlconnection object
+//            URLConnection urlConnection = url.openConnection();
+//
+//            // wrap the urlconnection in a bufferedreader
+//            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+//
+//            String line;
+//
+//            // read from the urlconnection via the bufferedreader
+//            while ((line = bufferedReader.readLine()) != null){
+//                content.append(line + "\n");
+//            }
+//            bufferedReader.close();
+//        }
+//        catch(Exception e)    {
+//            e.printStackTrace();
+//        }
+//        return content.toString();
+//    }
 }
